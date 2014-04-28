53effafa3d6b0ba3e688bb1314583ef7ef6ab4ec
Keri Clowes <kerilyn.clowes@gmail.com>
2014-04-28 11:57:02 -0600

    Databases and first migration completed

---
+++ db/migrate/20140428175345_create_videos.rb
@@ -0,0 +1,9 @@
+class CreateVideos < ActiveRecord::Migration
+  def change
+    create_table :videos do |t|
+      t.string :url
+      t.string :description
+      t.integer :rating
+    end
+  end
+end
