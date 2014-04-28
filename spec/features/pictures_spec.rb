8f02ba38b4a682b13bbe22f54817b709fcf03a33
Scott Skender <sjskender@gmail.com>
2014-04-28 16:45:51 -0600

    [#70288698] Line breaks in description are displayed
    Signed-off-by: Steve Magelowitz <steven.magelowitz@gmail.com>

--- spec/features/pictures_spec.rb
+++ spec/features/pictures_spec.rb
@@ -7,14 +7,14 @@ feature 'picture management' do
     click_link 'New Picture'
 
     fill_in 'url', with: "http://wac.450f.edgecastcdn.net/80450F/kyssfm.com/files/2012/10/cat-4-Brit.jpg"
-    fill_in 'description', with: "Cat"
+    fill_in 'description', with: "Cats\nThey are neat."
     fill_in 'rating', with: "4"
 
     click_button "Create Picture"
 
     expect(page).to have_content "Picture successfully created"
     expect(page).to have_content "http://wac.450f.edgecastcdn.net/80450F/kyssfm.com/files/2012/10/cat-4-Brit.jpg"
-    expect(page).to have_content "Cat"
+    expect(page).to have_content "Cats\nThey are neat."
     expect(page).to have_content "Rating: 4 out of 5"
     page.should have_css("img[src='http://wac.450f.edgecastcdn.net/80450F/kyssfm.com/files/2012/10/cat-4-Brit.jpg']")
 
@@ -45,7 +45,7 @@ feature 'picture management' do
 
     expect(page).to have_no_content "Picture successfully created"
     expect(page).to have_content "http://wac.450f.edgecastcdn.net/80450F/kyssfm.com/files/2012/10/cat-4-Brit.jpg"
-    expect(page).to have_content "Cat"
+    expect(page).to have_content "Cats\nThey are neat."
     expect(page).to have_content "Rating: 4 out of 5"
     page.should have_css("img[src='http://wac.450f.edgecastcdn.net/80450F/kyssfm.com/files/2012/10/cat-4-Brit.jpg']")
   end
