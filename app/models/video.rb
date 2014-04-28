843a8a81de866bb51ab58a120b279551cdaae24f
Keri Clowes <kerilyn.clowes@gmail.com>
2014-04-28 16:07:37 -0600

    [#70288664] Users cannot add videos that do not come from youtube
    Signed-off-by: Bebe Peng <bebepeng@gmail.com>

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,6 @@
 class Video < ActiveRecord::Base
   validates :url, presence: true
+  validates :url, format: {
+    with: /(https?:\/\/youtu.be\/)|(https?:\/\/www.youtube.com\/watch\?v=)/,
+    message: "Url must come from Youtube"}
 end
