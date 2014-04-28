d34f9a042cdaeebbeb8c212e4576cec42f356296
Keri Clowes <kerilyn.clowes@gmail.com>
2014-04-28 14:38:36 -0600

    [#70288658] Users can create a new video
    Signed-off-by: Bebe Peng <bebepeng@gmail.com>

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,4 +5,17 @@ class VideosController < ApplicationController
   def new
     @video = Video.new
   end
+
+  def create
+    @video = Video.new
+    @video.url = params[:video][:url]
+    @video.description = params[:video][:description]
+    @video.rating = params[:video][:rating]
+    @video.save
+    redirect_to "/videos/#{@video.id}"
+  end
+
+  def show
+    @video = Video.find(params[:id])
+  end
 end
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -17,7 +17,12 @@ feature 'welcome page' do
     click_on 'New Video'
 
     fill_in 'video_url', :with => 'https://www.youtube.com/watch?v=yTCRwi71_ns'
-    fill_in 'video_description', :with => 'Mit Romney'
+    fill_in 'video_description', :with => 'Mitt Romney'
     fill_in 'video_rating', :with => '3'
+    click_on 'Create Video'
+    expect(page).to have_content('Video successfully created')
+    expect(page).to have_content('https://www.youtube.com/watch?v=yTCRwi71_ns')
+    expect(page).to have_content('Mitt Romney')
+    expect(page).to have_content('3')
   end
 end
