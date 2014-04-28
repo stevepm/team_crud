6588f6f7c677476d92cea081132049558624545c
Keri Clowes <kerilyn.clowes@gmail.com>
2014-04-28 16:25:47 -0600

    [#70288666] user sees an error when the video description is blank
    Signed-off-by: Bebe Peng <bebepeng@gmail.com>

--- spec/features/videos_spec.rb
+++ spec/features/videos_spec.rb
@@ -16,6 +16,7 @@ feature 'video management' do
     expect(page).to have_content('3')
     expect(page).to have_selector 'iframe'
   end
+
   scenario 'users cannot add a video with a blank url' do
     visit '/videos/new'
 
@@ -37,4 +38,13 @@ feature 'video management' do
     expect(page).to_not have_selector 'iframe'
   end
 
+  scenario 'users cannot add a video with a blank description' do
+    visit '/videos/new'
+
+    fill_in 'video_url', :with => 'https://www.youtube.com/watch?v=yTCRwi71_ns'
+    fill_in 'video_rating', :with => '3'
+    click_on 'Create Video'
+    expect(page).to have_content("Description can't be blank")
+    expect(page).to_not have_selector 'iframe'
+  end
 end
