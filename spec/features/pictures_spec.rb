35c2bfa372d28d5100da91289501763f7e419ec3
Scott Skender <sjskender@gmail.com>
2014-04-28 14:51:47 -0600

    [#70288692] User sees an error when they create a picture without a description
    Signed-off-by: Steve Magelowitz <steven.magelowitz@gmail.com>

--- spec/features/pictures_spec.rb
+++ spec/features/pictures_spec.rb
@@ -10,7 +10,7 @@ feature 'picture management' do
     fill_in 'description', with: "Cat"
     fill_in 'rating', with: "4"
 
-    click_button "Add Picture"
+    click_button "Create Picture"
 
     expect(page).to have_content "Picture successfully created"
     expect(page).to have_content "http://wac.450f.edgecastcdn.net/80450F/kyssfm.com/files/2012/10/cat-4-Brit.jpg"
@@ -23,13 +23,13 @@ feature 'picture management' do
     click_link 'New Picture'
 
     fill_in 'url', with: ""
-    fill_in 'description', with: "Cat"
+    fill_in 'description', with: ""
     fill_in 'rating', with: "4"
-    click_button "Add Picture"
+    click_button "Create Picture"
 
     expect(current_url).to eq('http://www.example.com/pictures')
     expect(page).to have_content('URL cannot be blank')
-    expect(page).to have_content('Cat')
+    expect(page).to have_content('Description cannot be blank')
     page.should have_css("input[value='4']")
   end
 end
