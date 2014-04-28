15edf552287d78b4794ab59513afdc8efc0212fe
Keri Clowes <kerilyn.clowes@gmail.com>
2014-04-28 16:40:46 -0600

    [#70288668] Users can see links for all the videos
    Signed-off-by: Bebe Peng <bebepeng@gmail.com>

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,6 @@
 class VideosController < ApplicationController
   def index
+    @videos = Video.all
   end
 
   def new
--- spec/features/videos_spec.rb
+++ spec/features/videos_spec.rb
@@ -47,4 +47,26 @@ feature 'video management' do
     expect(page).to have_content("Description can't be blank")
     expect(page).to_not have_selector 'iframe'
   end
+
+  scenario 'user can see links for all of the videos displayed' do
+    visit '/'
+
+    click_on 'All Videos'
+    click_on 'New Video'
+
+    fill_in 'video_url', :with => 'https://www.youtube.com/watch?v=yTCRwi71_ns'
+    fill_in 'video_description', :with => 'Mitt Romney'
+    fill_in 'video_rating', :with => '3'
+    click_on 'Create Video'
+    visit '/videos/new'
+    fill_in 'video_url', :with => 'http://youtu.be/OydqR_7_DjI'
+    fill_in 'video_description', :with => 'No glasses'
+    fill_in 'video_rating', :with => '5'
+    click_on 'Create Video'
+
+    visit '/'
+    click_on 'All Videos'
+    expect(page).to have_content 'https://www.youtube.com/watch?v=yTCRwi71_ns'
+    expect(page).to have_content 'http://youtu.be/OydqR_7_DjI'
+  end
 end
