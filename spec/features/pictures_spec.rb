b81a690b4c411238ff11e99fa3d910bfab89d6f0
Keri Clowes <kerilyn.clowes@gmail.com>
2014-04-29 15:09:20 -0600

    [finish #70288708 #70288706] user can delete a photo and all photos will show up on the pictures homepage
    Signed-off-by: Steve Magelowitz <steven.magelowitz@gmail.com>

--- spec/features/pictures_spec.rb
+++ spec/features/pictures_spec.rb
@@ -117,4 +117,34 @@ feature 'picture management' do
     expect(page).to have_content "Cats\nThey are super neat."
     expect(page).to have_content "Rating: 5 out of 5"
   end
+
+  scenario 'User can delete a picture' do
+    visit '/pictures'
+
+    click_link 'New Picture'
+
+    fill_in 'url', with: "http://wac.450f.edgecastcdn.net/80450F/kyssfm.com/files/2012/10/cat-4-Brit.jpg"
+    fill_in 'description', with: "Cats\nThey are neat."
+    fill_in 'rating', with: "4"
+    click_button "Create Picture"
+
+    click_link 'Delete'
+
+    page.should_not have_css("img[src='http://wac.450f.edgecastcdn.net/80450F/kyssfm.com/files/2012/10/cat-4-Brit.jpg']")
+  end
+
+  scenario 'User can view all images on homepage' do
+    visit '/pictures'
+
+    click_link 'New Picture'
+
+    fill_in 'url', with: "http://wac.450f.edgecastcdn.net/80450F/kyssfm.com/files/2012/10/cat-4-Brit.jpg"
+    fill_in 'description', with: "Cats\nThey are neat."
+    fill_in 'rating', with: "4"
+    click_button "Create Picture"
+
+    visit '/pictures'
+    page.should have_css("img[src='http://wac.450f.edgecastcdn.net/80450F/kyssfm.com/files/2012/10/cat-4-Brit.jpg']")
+
+  end
 end
